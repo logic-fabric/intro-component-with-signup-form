4996a52b218b20203cb6cbf6477f1b1e65f1762c
logic-fabric <lowic@logic-fabric.fr>
2024-02-08 17:38:36 +0100

    refactor (form): add fields events listeners using a loop

--- js/index.js
+++ js/index.js
@@ -4,41 +4,22 @@ import {
   hideErrorMessage,
 } from "./actions.js";
 
+const FORM_FIELDS = ["first-name", "last-name", "email", "password"];
+
 const tryItFreeButton = document.getElementById("try-it-free-button");
 
 tryItFreeButton.addEventListener("click", () => {
   giveFocusTo(firstNameInput);
 });
 
-const firstNameInput = document.getElementById("first-name");
-const lastNameInput = document.getElementById("last-name");
-const emailInput = document.getElementById("email");
-const passwordInput = document.getElementById("password");
+for (const field of FORM_FIELDS) {
+  const input = document.getElementById(field);
 
-firstNameInput.addEventListener("blur", () => {
-  checkInputValidity("first-name");
-});
-firstNameInput.addEventListener("focus", () => {
-  hideErrorMessage("first-name");
-});
+  input.addEventListener("blur", () => {
+    checkInputValidity(field);
+  });
 
-lastNameInput.addEventListener("blur", () => {
-  checkInputValidity("last-name");
-});
-lastNameInput.addEventListener("focus", () => {
-  hideErrorMessage("last-name");
-});
-
-emailInput.addEventListener("blur", () => {
-  checkInputValidity("email");
-});
-emailInput.addEventListener("focus", () => {
-  hideErrorMessage("email");
-});
-
-passwordInput.addEventListener("blur", () => {
-  checkInputValidity("password");
-});
-passwordInput.addEventListener("focus", () => {
-  hideErrorMessage("password");
-});
+  input.addEventListener("focus", () => {
+    hideErrorMessage(field);
+  });
+}
